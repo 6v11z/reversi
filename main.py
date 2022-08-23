c20d8bcb5ad4bb9a70b7d5a9da78199de65c4e93
Nacho <Nacho@MacBook-Air-de-javiera.local>
2022-08-23 18:11:46 -0400

    agregando get_row_col_mouse()

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from reversi.board import Board
-from reversi.constants import WIDTH, HEIGTH, COLOR_GREEN
+from reversi.constants import COLOR_BLACK, SQUARE_SIZE, WIDTH, HEIGTH, COLOR_GREEN, ROWS
+from reversi.game import Game
 
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGTH))
 WIN.fill(COLOR_GREEN)
 pygame.display.set_caption("Reversi")
 
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = int(y // SQUARE_SIZE)
+    col = int(x // SQUARE_SIZE)
+    return (row, col)
+
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
@@ -19,11 +26,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                print(pos)
-        board.drawGrid(WIN)
-        board.draw(WIN)
-        pygame.display.update()
+                pos = get_row_col_from_mouse(pygame.mouse.get_pos())
+                row = pos[0]
+                col = pos[1]
+                # board.add_piece(row, col)
+        game.update()
 
     pygame.quit()
  
