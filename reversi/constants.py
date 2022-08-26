eda2ecee4c9ea9ed17dabba5f60b73f2b38cdfc3
6vllz <luisitm2000@gmail.com>
2022-08-25 22:57:12 -0400

    agregando la variable global DIRECCIONES

--- reversi/constants.py
+++ reversi/constants.py
@@ -8,3 +8,5 @@ SQUARE_SIZE = WIDTH / ROWS
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_GREEN = (0, 200, 0)
+
+DIRECTIONS = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
